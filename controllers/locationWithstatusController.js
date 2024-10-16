65adbcf4fa82bd2edcde647df296968539fbc543
they-call-me-E <jakemohl@gmail.com>
2024-10-16 15:55:54 +0600

    add forgotPassword feature

--- controllers/locationWithstatusController.js
+++ controllers/locationWithstatusController.js
@@ -1,4 +1,7 @@
-module.exports.updateLocationWithStatus = catchasync(async (req, res, next) => {
+const catchAsync = require("../utils/catchasync");
+const AppError = require("./../utils/apperror");
+
+module.exports.updateLocationWithStatus = catchAsync(async (req, res, next) => {
   // Check existing user
   const existing_user = await User.findById(req.params.id);
 
@@ -7,37 +10,61 @@ module.exports.updateLocationWithStatus = catchasync(async (req, res, next) => {
   }
 
   if (req.params.id !== JSON.stringify(req.user._id).replace(/"/g, "")) {
-    return next(new AppError("You do not have permission for this action", 403));
+    return next(
+      new AppError("You do not have permission for this action", 403)
+    );
   }
 
   // Status updated code start
   const statusBody = { status: { device: {} } };
 
-  if (req?.body?.status?.location_sharing !== undefined && req?.body?.status?.location_sharing !== null) {
+  if (
+    req?.body?.status?.location_sharing !== undefined &&
+    req?.body?.status?.location_sharing !== null
+  ) {
     statusBody.status.location_sharing = req.body.status.location_sharing;
   }
 
-  if (req?.body?.status?.isMoving !== undefined && req?.body?.status?.isMoving !== null) {
+  if (
+    req?.body?.status?.isMoving !== undefined &&
+    req?.body?.status?.isMoving !== null
+  ) {
     statusBody.status.isMoving = req.body.status.isMoving;
   }
 
-  if (req?.body?.status?.speed !== undefined && req?.body?.status?.speed !== null) {
+  if (
+    req?.body?.status?.speed !== undefined &&
+    req?.body?.status?.speed !== null
+  ) {
     statusBody.status.speed = req.body.status.speed;
   }
 
-  if (req?.body?.status?.device?.screen !== undefined && req?.body?.status?.device?.screen !== null) {
+  if (
+    req?.body?.status?.device?.screen !== undefined &&
+    req?.body?.status?.device?.screen !== null
+  ) {
     statusBody.status.device.screen = req.body.status.device.screen;
   }
 
-  if (req?.body?.status?.device?.wifi !== undefined && req?.body?.status?.device?.wifi !== null) {
+  if (
+    req?.body?.status?.device?.wifi !== undefined &&
+    req?.body?.status?.device?.wifi !== null
+  ) {
     statusBody.status.device.wifi = req.body.status.device.wifi;
   }
 
-  if (req?.body?.status?.device?.battery_level !== undefined && req?.body?.status?.device?.battery_level !== null) {
-    statusBody.status.device.battery_level = req.body.status.device.battery_level;
+  if (
+    req?.body?.status?.device?.battery_level !== undefined &&
+    req?.body?.status?.device?.battery_level !== null
+  ) {
+    statusBody.status.device.battery_level =
+      req.body.status.device.battery_level;
   }
 
-  if (req?.body?.status?.device?.charging !== undefined && req?.body?.status?.device?.charging !== null) {
+  if (
+    req?.body?.status?.device?.charging !== undefined &&
+    req?.body?.status?.device?.charging !== null
+  ) {
     statusBody.status.device.charging = req.body.status.device.charging;
   }
   // Status updated code end
@@ -45,15 +72,24 @@ module.exports.updateLocationWithStatus = catchasync(async (req, res, next) => {
   // Location updated code start
   const locationBody = { location: {} };
 
-  if (req?.body?.location?.latitude !== undefined && req?.body?.location?.latitude !== null) {
+  if (
+    req?.body?.location?.latitude !== undefined &&
+    req?.body?.location?.latitude !== null
+  ) {
     locationBody.location.latitude = req.body.location.latitude;
   }
 
-  if (req?.body?.location?.longitude !== undefined && req?.body?.location?.longitude !== null) {
+  if (
+    req?.body?.location?.longitude !== undefined &&
+    req?.body?.location?.longitude !== null
+  ) {
     locationBody.location.longitude = req.body.location.longitude;
   }
 
-  if (req?.body?.location?.address !== undefined && req?.body?.location?.address !== null) {
+  if (
+    req?.body?.location?.address !== undefined &&
+    req?.body?.location?.address !== null
+  ) {
     locationBody.location.address = req.body.location.address;
   }
   // Location updated code end
