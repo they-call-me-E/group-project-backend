10ce10838cf8401ab7a45d7e7662e264b9579886
they-call-me-E <64619110+they-call-me-E@users.noreply.github.com>
2024-10-28 22:32:46 -0400

    Update user.js
    Reverted a commit

--- models/user.js
+++ models/user.js
@@ -1,87 +1,179 @@
-const catchAsync = require("../utils/catchasync");
-const AppError = require("./../utils/apperror");
-const { User } = require("../models/user");
 
-module.exports.updateLocationWithStatus = catchAsync(async (req, res, next) => {
-  const isValidField = (field) => field !== undefined && field !== null;
+@@ -1,178 +1,178 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const validator = require("validator");
+const { v4: uuidv4 } = require("uuid");
+const crypto = require("crypto");
 
-  const existing_user = await User.findById(req.params.id);
-  if (!existing_user) {
-    return next(new AppError("No user found with that ID", 404));
-  }
-
-  if (req.params.id !== req.user._id.toString()) {
-    return next(new AppError("You do not have permission for this action", 403));
-  }
-
-  const updateFields = {};
-
-  if (isValidField(req.body.relation)) {
-    updateFields["relation"] = req.body.relation;
-  }
-
-  let locationUpdated = false;
-  if (isValidField(req.body.location?.latitude)) {
-    updateFields["location.latitude"] = req.body.location.latitude;
-    locationUpdated = true;
-  }
-  if (isValidField(req.body.location?.longitude)) {
-    updateFields["location.longitude"] = req.body.location.longitude;
-    locationUpdated = true;
-  }
-  if (isValidField(req.body.location?.address)) {
-    updateFields["location.address"] = req.body.location.address;
-    locationUpdated = true;
-  }
-
-  // Update timestamp if location fields are modified
-  if (locationUpdated) {
-    updateFields["location.timestamp"] = Date.now();
-  }
-
-  if (isValidField(req.body.status?.location_sharing)) {
-    updateFields["status.location_sharing"] = req.body.status.location_sharing;
-  }
-  if (isValidField(req.body.status?.isMoving)) {
-    updateFields["status.isMoving"] = req.body.status.isMoving;
-  }
-  if (isValidField(req.body.status?.speed)) {
-    updateFields["status.speed"] = req.body.status.speed;
-  }
-
-  const deviceStatus = req.body.status?.device;
-  if (deviceStatus) {
-    if (isValidField(deviceStatus.screen)) {
-      updateFields["status.device.screen"] = deviceStatus.screen;
-    }
-    if (isValidField(deviceStatus.wifi)) {
-      updateFields["status.device.wifi"] = deviceStatus.wifi;
-    }
-    if (isValidField(deviceStatus.battery_level)) {
-      updateFields["status.device.battery_level"] = deviceStatus.battery_level;
-    }
-    if (isValidField(deviceStatus.charging)) {
-      updateFields["status.device.charging"] = deviceStatus.charging;
-    }
-    if (isValidField(deviceStatus.currentApp)) {
-      updateFields["status.device.currentApp"] = deviceStatus.currentApp;
-    }
-  }
-
-  const updatedUser = await User.findByIdAndUpdate(
-    req.user.id,
-    { $set: updateFields },
-    {
-      new: true,
-      runValidators: true,
-    }
-  );
-
-  res.status(200).json({
-    user: {
-      status: updatedUser.status,
-      location: updatedUser.location,
-      relation: updatedUser.relation,
+const userSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: String,
+      default: uuidv4,
     },
-  });
+    name: {
+      type: String,
+      required: [true, "Please tell us your name!"],
+      minlength: 1,
+      maxlength: 50,
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide your email"],
+      unique: true,
+      lowercase: true,
+      validate: [validator.isEmail, "Please Provide Your valid email"],
+    },
+    inviteGroupsList: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "Group",
+        required: true,
+      },
+    ],
+    phone: {
+      type: String,
+    },
+    relation: {
+      type: String,
+    },
+    avatar: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: [true, "Please Provide a password"],
+      minlength: 8,
+      select: false,
+    },
+    passwordConfirm: {
+      type: String,
+      required: [true, "Please Confirm your password"],
+      validate: {
+        validator: function (el) {
+          return el === this.password;
+        },
+        message: "Passwords are not the same",
+      },
+    },
+    location: {
+      latitude: {
+        type: Number,
+      },
+      longitude: {
+        type: Number,
+      },
+      address: {
+        type: String,
+      },
+      timestamp: {
+        type: String,
+      },
+    },
+    status: {
+      location_sharing: {
+        type: Boolean,
+      },
+      isMoving: {
+        type: Boolean,
+      },
+      speed: {
+        type: Number,
+      },
+      device: {
+        screen: {
+          type: Boolean,
+        },
+        wifi: {
+          type: Boolean,
+        },
+        battery_level: {
+          type: Number,
+        },
+        charging: {
+          type: Boolean,
+        },
+        currentApp: {
+          type: String,
+        },
+      },
+    },
+    // Geodata for geofence tracking
+    geodata: [
+      {
+        _id: {
+          type: String,
+          default: uuidv4,
+        },
+        currentGeofenceId: {
+          type: String,
+          required: true, // Geofence ID is required
+          // my code (shakib)
+          ref: "Fences",
+        },
+        groupId: {
+          type: String,
+          required: true, // Group ID is required
+          ref: "Group",
+        },
+        geofenceName: {
+          type: String,
+          required: [true, "geofenceName is required"], // Geofence name is mandatory
+        },
+        enteredAt: {
+          type: Date,
+          default: null, // Set when the user enters a geofence
+        },
+      },
+    ],
+    passwordResetToken: String,
+    passwordResetExpires: Date,
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  return next();
 });
+userSchema.pre("save", function (next) {
+  if (!this.isModified("password") || this.isNew) return next();
+  this.passwordChangedAt = Date.now() - 1000;
+  return next();
+});
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+    return JWTTimestamp < changedTimestamp;
+  }
+  // False means Not changed
+  return false;
+};
+
+userSchema.methods.creatPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString("hex");
+
+  this.passwordResetToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  this.passwordResetExpires = Date.now() + 30 * 60 * 1000;
+  return resetToken;
+};
+
+module.exports.User = mongoose.model("User", userSchema);
