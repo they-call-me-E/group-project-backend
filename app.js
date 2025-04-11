a191650584527e0a5e5ab53f1f796947acd0862d
they-call-me-E <64619110+they-call-me-E@users.noreply.github.com>
2025-04-11 23:56:02 +0600

    hiding temporary rate limit rule code

--- app.js
+++ app.js
@@ -37,11 +37,11 @@ app.use(
 );
 
 // Create a rate limit rule
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
-  message: "Too many requests from this IP, please try again after 15 minutes.",
-});
+// const limiter = rateLimit({
+//  windowMs: 15 * 60 * 1000, // 15 minutes
+//  max: 100, // Limit each IP to 100 requests per windowMs
+//  message: "Too many requests from this IP, please try again after 15 minutes.",
+// });
 
 // Apply the rate limiter to all requests
 app.use(limiter);
