d8da68914617d17691e4b8d32d4ca34ff0e9638a
they-call-me-E <64619110+they-call-me-E@users.noreply.github.com>
2025-04-23 12:15:29 +0600

    resolve user avatar update issue and initialize cron job for scheduled updates

--- utils/awsS3.js
+++ utils/awsS3.js
@@ -1,6 +1,7 @@
 const { S3Client } = require("@aws-sdk/client-s3");
 
 module.exports.BUCKET_NAME = process.env.AWS_BUCKET_NAME;
+
 // AWS S3 Client
 module.exports.s3 = new S3Client({
   region: process.env.AWS_REGION,
