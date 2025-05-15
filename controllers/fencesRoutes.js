f7ba19a652d9a53b8db2d1220a1a723e463cd180
they-call-me-E <64619110+they-call-me-E@users.noreply.github.com>
2025-05-15 15:14:20 -0400

    Update fencesRoutes.js

--- controllers/fencesRoutes.js
+++ controllers/fencesRoutes.js
@@ -1,11 +1,1 @@
-const express = require("express");
-const { protect } = require("../controllers/authController");
-const { getAllFences } = require("../controllers/fencesController");
 
-const router = express.Router();
-
-router.use(protect);
-
-router.get("/", getAllFences);
-
-module.exports = router;
