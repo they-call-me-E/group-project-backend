811ee7ea6fb2fafcf68c73e0d8f7de72096d5f37
they-call-me-E <64619110+they-call-me-E@users.noreply.github.com>
2025-05-31 23:38:18 +0600

    superuser feature done

--- routes/grouproutes.js
+++ routes/grouproutes.js
@@ -40,19 +40,19 @@ router.use(isSuperUserOnly);
 router.get("/:groupId/members/:userId", getAllGroupDataWithMembers); // superuser with validation feature done
 router.get("/:id/members", getAllMembers); // superuser with validation feature done
 router.get("/:id/admins", getAllAdmins); // superuser with validation feature done
-router.get("/", getAllgroups); // waiting for response
+router.get("/", getAllgroups); // superuser with validation feature done
 router.get("/:id", getSingleGroup); // superuser with validation feature done
 router.post("/create", createGroup); // superuser with all validation feature done
 router.patch("/:id/add/members", addMembers); //superuser with all validation feature done
 router.patch("/:id/add/admin", addAdmin); // superuser with all validation feature done
 router.patch("/:id/remove/admin", removeAdmin); // superuser with all validation feature done
 router.patch("/:id/remove/members", removeMembers); // superuser with all validation feature done
-router.delete("/:id", deleteGroup); // all validation feature done
-router.patch("/:id", updateGroup); // all validation feature done
-router.patch("/leave/:id", leaveMembers); // processing and all validation feature done
-router.get("/:id/fences", getGroupFences); // validation feature done
-router.get("/:groupId/fences/:fenceId", getSingleGroupFences); // validation feature done
-router.post("/:groupId/invite/generate", generateInviteCode); // all validation feature done
-router.post("/invite/join", joinGroupWithInvite); // all validation feature done
+router.delete("/:id", deleteGroup); // superuser with all validation feature done
+router.patch("/:id", updateGroup); // superuser superuser all validation feature done
+router.patch("/leave/:id", leaveMembers); //  superuser with and all validation feature done
+router.get("/:id/fences", getGroupFences); // superuser with validation feature done
+router.get("/:groupId/fences/:fenceId", getSingleGroupFences); // superuser with validation feature done
+router.post("/:groupId/invite/generate", generateInviteCode); // I think Here is no need to implement superuser feature all validation feature done
+router.post("/invite/join", joinGroupWithInvite); //I think Here is no need to implement superuser feature all validation feature done
 
 module.exports = router;
