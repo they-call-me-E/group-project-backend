45a18658e58c281be32705516060f4d8926b3c4e
they-call-me-E <64619110+they-call-me-E@users.noreply.github.com>
2025-05-31 23:28:31 +0600

    superuser feature done

--- controllers/errorcontroller.js
+++ controllers/errorcontroller.js
@@ -13,6 +13,10 @@ const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}.`;
   return new AppError(message, 400);
 };
+const handleJWTError = (err) =>
+  new AppError("Invalid token. Please log in again", 401);
+const handleJWTExpiredError = (err) =>
+  new AppError("Your token has expired! please log in again!", 401);
 
 //development error middleware
 function developmentError(err, req, res) {
@@ -56,6 +60,12 @@ module.exports = (err, req, res, next) => {
       productionError(error, req, res);
     } else if (err?.name === "CastError") {
       let error = handleCastErrorDB(err);
+      productionError(error, req, res);
+    } else if (err.name === "JsonWebTokenError") {
+      let error = handleJWTError(err);
+      productionError(error, req, res);
+    } else if (err.name === "TokenExpiredError") {
+      let error = handleJWTExpiredError(err);
       productionError(error, req, res);
     } else {
       productionError(err, req, res);
