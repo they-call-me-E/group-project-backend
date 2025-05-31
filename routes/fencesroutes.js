0f107f581ca6cef007f69aa3c47015c366495720
they-call-me-E <64619110+they-call-me-E@users.noreply.github.com>
2025-05-31 23:37:52 +0600

    superuser feature done

--- routes/fencesroutes.js
+++ routes/fencesroutes.js
@@ -6,15 +6,17 @@ const {
   updateFences,
   deleteFences,
 } = require("./../controllers/fencesController");
+const { isSuperUserOnly } = require("../middleware/isSuperUserOnly");
 
 // Mounting multiple routers
 const router = express.Router();
 
 //Protect All routes after this middleware
 router.use(protect);
+router.use(isSuperUserOnly);
 
-router.post("/:id/fences/create", createFences); // all validation done
-router.patch("/:groupId/fences/:fenceId/update", updateFences); // all validation done
-router.delete("/:groupId/fences/:fenceId/delete", deleteFences); // all validation done
+router.post("/:id/fences/create", createFences); // superuser with all validation done
+router.patch("/:groupId/fences/:fenceId/update", updateFences); // superuser with all validation done
+router.delete("/:groupId/fences/:fenceId/delete", deleteFences); // superuser with all validation done
 
 module.exports = router;
