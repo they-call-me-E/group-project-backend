69b2f778eba84bab1bed9f703eefa2b6270fd78b
they-call-me-E <64619110+they-call-me-E@users.noreply.github.com>
2025-05-31 23:38:47 +0600

    superuser feature done

--- routes/locationWithstatusroutes.js
+++ routes/locationWithstatusroutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const { protect } = require("../controllers/authController");
+const { isSuperUserOnly } = require("../middleware/isSuperUserOnly");
 const {
   updateLocationWithStatus,
 } = require("./../controllers/locationWithstatusController");
@@ -10,7 +11,8 @@ const router = express.Router();
 
 //Protect All routes after this middleware
 router.use(protect);
+router.use(isSuperUserOnly);
 
-router.patch("/:id", updateLocationWithStatus); // all validation done
+router.patch("/:id", updateLocationWithStatus); // superuser with all validation done
 
 module.exports = router;
